a0961a3b2e103fd50da1b05fc864dd2e7ac4a136
Chris Jones <captjones@gmail.com>
2011-05-03 22:39:57 -0500

    Added README and first program, 21sticks

---
+++ 21sticks.rb
@@ -0,0 +1,75 @@
+class Game
+  def initialize(name)
+    @name = name
+    @total = 21
+    play_game
+  end
+  
+  def get_computer_number
+    choices, pick = [1, 2, 3, 4], @total + 1
+    while !valid_pick?(pick)
+      pick = choices[rand(choices.length)]
+    end
+    return pick
+  end
+  
+  def end_of_game?
+    @total == 0 ? true : false
+  end
+
+  def get_player_number
+    print "\n#{@name}, how many sticks would you like to remove? (1-4) "
+    s = gets.chomp!.to_i
+  end
+
+  def play_game
+    player_turn = true
+    while not end_of_game?
+      player_pick = get_player_number
+      if player_pick >= 1 && player_pick <= 4
+        if valid_pick?(player_pick)
+          @total -= player_pick
+          puts "You chose #{player_pick} #{plural!(player_pick)}. #{@total} #{plural!(@total)} remaining."
+          end_of_game? ? puts("Congratulations, you win!") : player_turn = false
+        else
+          puts "There are not that many sticks left. #{@total} sticks left."
+        end
+      else
+        puts "\nYou chose an invalid number. Please pick again."
+      end
+      if !player_turn
+        computer_pick = get_computer_number
+        @total -= computer_pick
+        puts "Computer chose #{computer_pick} #{plural!(computer_pick)}. #{@total} #{plural!(@total)} remaining."
+        end_of_game? ? puts("So sorry, computer wins. Better luck next time.") : player_turn = true
+      end
+    end
+  end
+
+  def plural!(number)
+    number != 1 ? "sticks" : "stick"
+  end
+
+  def valid_pick?(pick)
+    pick > @total ? false : true
+  end
+
+end
+
+response = ""
+print "Hello, welcome to the game 21sticks. What is your name? "
+name = gets.chomp!
+while response != 'q'
+  print "\nHello again #{name}, ready to play (y n)? To quit, press Q. "
+  response = gets.chomp.downcase
+  if response == 'n'
+    puts "Sleeping..."
+    sleep 5
+  elsif response == 'y'
+    game = Game.new(name)
+  elsif response == 'q'
+    puts "\nAwe sorry to see you go so soon, #{name}. See you next time.\n"
+  else
+    puts "Invalid response. Quit trying to be Kevin Mitnick and just play the damn game!\n"  
+  end
+end
